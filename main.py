adada8a27f3ce7e504f3a5b965cd0c47a3e0d2de
Lennard Walter <67687452+hallowed@users.noreply.github.com>
2020-07-08 18:08:53 +0200

    very small visual change because i'm perfectionistic
    carriage return before "wait until all threads are stopped" so we don't see the ugly keyboard interrupt ^C.

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
         # i could use sys and clear the line but I don't really want to use two ways of printing
         # and I'm to lazy to swap out everything
         # also, this works perfectly
-        print("[!] Warte, bis alle Threads gestoppt sind!" + 40*" ")
+        print("\r[!] Warte, bis alle Threads gestoppt sind!" + 40*" ")
         
         info_loop_stop = True
 
